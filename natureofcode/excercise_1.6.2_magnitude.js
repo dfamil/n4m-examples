68dd0fddee2b8693bd222e6ebf0119f82d45d9e8
dfamil <pulse2@verizon.net>
2019-01-18 10:30:06 -0800

    Update excercise_1.6.2_magnitude.js

--- natureofcode/excercise_1.6.2_magnitude.js
+++ natureofcode/excercise_1.6.2_magnitude.js
@@ -5,22 +5,20 @@ const maxAPI = require("max-api");
 var Victor = require("victor");
 var center = [];
 var mouse = [];
-// var vec2 = new Victor(0, 0);
-maxAPI.addHandler("vectorNormlize", (...args) => 
-	{
+
+maxAPI.addHandler("vectorNormlize", (...args) =>
+{
 	// vec2 = new Victor(0, 0);
 	mouse = new Victor(args[0], args[1]);
 	center = new Victor(300, 200);
 	// var multiy = new Victor(150, 150);
 	// var vec1 = new Victor(317, 15);
-	var test = new Victor(100, 100);
+
 	mouse.subtract(center);
 	// var vec2 = test.normalize();
 	var vec3 = mouse.normalize();
 	// maxAPI.outlet("normVector",vec3.toString());
 	// maxAPI.outlet("normVectorLenght",vec3.length());
-
-	var multiply = vec3.multiply(multiy);
 
 	console.log(...args);
 	console.log(vec3.toString());
