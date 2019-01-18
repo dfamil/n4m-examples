56a4a7c2c36a9c1a3abf2c5dd7fc7de714d80fbd
dfamil <pulse2@verizon.net>
2019-01-18 07:12:26 -0800

    Update excercise_1.7_motion101_moverClass.js

--- natureofcode/excercise_1.7_motion101_moverClass.js
+++ natureofcode/excercise_1.7_motion101_moverClass.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// Require the max-api module to connect to Max via node.script
+// Require the max-api module to connect to Max via node.script 
 const maxAPI = require("max-api");
 var Victor = require('victor')
 const random = require('random')
@@ -13,44 +13,42 @@ var velocity = new Victor(random.float( -2, 2), random.float(-2, 2));
 
 maxAPI.addHandler("motion", (...args) => 
 	{
-        var mover = new Mover();
-        mover.update();
-        mover.checkEdges();
-        mover.display(); 
-     
+		var mover = new Mover();
+		mover.update();
+		mover.checkEdges();
+		mover.display(); 
+	 
 });
 
-
-
 class Mover{
-    constructor() 
-    {
-        var location = new Victor(random.float(0, 600), random.float(0, 200));
-        var velocity = new Victor(random.float( -2, 2), random.float(-2, 2));
-        var topspeed = 6;
-    }
-    update() 
-    {
-        location.add(velocity);
-    }
-    
-    display()
-    {
-        maxAPI.outlet("newLocationOutput",location.x,location.y);
-    }
-    checkEdges() {
+	constructor() 
+	{
+		var location = new Victor(random.float(0, 600), random.float(0, 200));
+		var velocity = new Victor(random.float( -2, 2), random.float(-2, 2));
+		var topspeed = 6;
+	}
+	update() 
+	{
+		location.add(velocity);
+	}
+	
+	display()
+	{
+		maxAPI.outlet("newLocationOutput",location.x, location.y);
+	}
+	checkEdges() {
 
-        if (location.x > width) {
-        location.x = 0;
-        } 
-        else if (location.x < 0) {
-        location.x = width;
-        }
-        if (location.y > height) {
-        location.y = 0;
-        } 
-        else if (location.y < 0) {
-        location.y = height;
-        }
-    }
+		if (location.x > width) {
+		location.x = 0;
+		} 
+		else if (location.x < 0) {
+		location.x = width;
+		}
+		if (location.y > height) {
+		location.y = 0;
+		} 
+		else if (location.y < 0) {
+		location.y = height;
+		}
+	}
 }
