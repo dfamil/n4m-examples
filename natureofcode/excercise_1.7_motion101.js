6392319fcfb071355f732520d7515cef19b1248b
dfamil <pulse2@verizon.net>
2019-01-18 08:05:37 -0800

    Update excercise_1.7_motion101.js

--- natureofcode/excercise_1.7_motion101.js
+++ natureofcode/excercise_1.7_motion101.js
@@ -1,13 +1,13 @@
 "use strict";
 
-// Require the max-api module to connect to Max via node.script 
+// Require the max-api module to connect to Max via node.script
 const maxAPI = require("max-api");
 var Victor = require('victor')
 const random = require('random')
 
 var width = 600;
 var height = 400
-//var vec2 = new Victor(0,0);
+// var vec2 = new Victor(0,0);
 var location = new Victor(random.float(0, 600), random.float(0, 200));
 var velocity = new Victor(random.float( -2, 2), random.float(-2, 2));
 	
@@ -30,11 +30,11 @@ maxAPI.addHandler("motion", (...args) =>
 	}else if (location.y < 0) {
 		location.y = height;
 	}
-	//maxAPI.outlet("newLocationOutput", 1550,1 200); 
+	// maxAPI.outlet("newLocationOutput", 1550,1 200); 
 	maxAPI.outlet("newLocationOutput",location.x,location.y, 5);
 	
-	//new Circle(0);
-	//Circle.PI = 3.14159;
+	// new Circle(0);
+	// Circle.PI = 3.14159;
 	Circle.prototype.area = Circle_area;
 	Circle.PI = 3.14159;
 	var c = new Circle(1.0); // create an instance of the Circle class  
