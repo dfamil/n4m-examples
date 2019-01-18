edf4b36ebc13a09c90cf17e69f44d610c96cf001
dfamil <pulse2@verizon.net>
2019-01-18 08:11:44 -0800

    Update excercise_1.4_Vector.js

--- natureofcode/excercise_1.4_Vector.js
+++ natureofcode/excercise_1.4_Vector.js
@@ -4,19 +4,19 @@
 const maxAPI = require("max-api");
 // var add = require('vectors/add')(2);
 // var mag = require('vectors/mag')(2)
-var sub = require('vectors/sub')(2);
+var sub = require("vectors/sub")(2);
 // var dot = require('dot-object');
 // var location = [100, 100];
 // var velocity = [2.5, 5];
-var center = [600, 400]
-// var mouse = []; 
-maxAPI.addHandler("locationVelocity", (...args) => 
+var center = [600, 400];
+// var mouse = [];
+maxAPI.addHandler("locationVelocity", (...args) =>
 {
 	var mouse = (args[0], args[1]);
-	sub(mouse,center);
+	sub(mouse, center);
 
 	// fill(0);
-	// rect(0,0,m, 10); 
+	// rect(0,0,m, 10);
 	// translate(width/2, height/2);
 	// line(0,0,mouse.x, mouse.y);
 	maxAPI.outlet("posX", mouse[0]);
