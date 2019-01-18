acf6a6237a8b7b1061b890a8ffc2ae6e8ae657ee
dfamil <pulse2@verizon.net>
2019-01-18 10:30:10 -0800

    Update excercise_1.7_motion101_moverClass.js

--- natureofcode/excercise_1.7_motion101_moverClass.js
+++ natureofcode/excercise_1.7_motion101_moverClass.js
@@ -2,53 +2,56 @@
 
 // Require the max-api module to connect to Max via node.script
 const maxAPI = require("max-api");
-var Victor = require("victor")
-const random = require("random")
+// var Victor = require("victor");
+// const random = require("random");
 
-// var width = 600;
-// var height = 400
+var width = 600;
+var height = 400;
 // var vec2 = new Victor(0,0);
 // var location = new Victor(random.float(0, 600), random.float(0, 200));
 // var velocity = new Victor(random.float( -2, 2), random.float(-2, 2));
 
-maxAPI.addHandler("motion", (...args) => 
+class Mover {
+	constructor()
 	{
-		var mover = new Mover();
-		mover.update();
-		mover.checkEdges();
-		mover.display();
-	
-});
+		// var location = new Victor(random.float(0, 600), random.float(0, 200));
+		// var velocity = new Victor(random.float( -2, 2), random.float(-2, 2));
+		// var topspeed = 6;
+	}
+	update()
+	{
+		// location.add(velocity);
+	}
 
-class Mover{
-	constructor() 
-	{
-		var location = new Victor(random.float(0, 600), random.float(0, 200));
-		var velocity = new Victor(random.float( -2, 2), random.float(-2, 2));
-		var topspeed = 6;
-	}
-	update() 
-	{
-		location.add(velocity);
-	}
-	
 	display()
 	{
-		maxAPI.outlet("newLocationOutput",location.x, location.y);
+		maxAPI.outlet("newLocationOutput", location.x, location.y);
 	}
+
 	checkEdges() {
 
 		if (location.x > width) {
-		location.x = 0;
-		} 
+			location.x = 0;
+		}
 		else if (location.x < 0) {
-		location.x = width;
+			location.x = width;
 		}
 		if (location.y > height) {
-		location.y = 0;
-		} 
+			location.y = 0;
+		}
 		else if (location.y < 0) {
-		location.y = height;
+			location.y = height;
 		}
 	}
 }
+
+
+maxAPI.addHandler("motion", (...args) =>
+{
+	var mover = new Mover();
+	mover.update();
+	mover.checkEdges();
+	mover.display();
+
+});
+
